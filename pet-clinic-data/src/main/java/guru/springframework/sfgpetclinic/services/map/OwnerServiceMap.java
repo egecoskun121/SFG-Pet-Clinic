843185fc65519989e64162daff6acce67290b6bc
egecoskun121 <77891949+egecoskun121@users.noreply.github.com>
2022-06-29 16:53:45 +0300

    Fixed service bugs and implemented DataLoader.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -1,11 +1,12 @@
 package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Owner;
-import guru.springframework.sfgpetclinic.services.CrudService;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements CrudService<Owner,Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements OwnerService {
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -14,6 +15,7 @@ public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements C
     public Owner findById(Long id) {
         return super.findById(id);
     }
+
     @Override
     public void deleteById(Long id) {
         super.deleteById(id);
@@ -31,4 +33,8 @@ public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements C
     }
 
 
+    @Override
+    public Owner findByLastName(String lastname) {
+        return null;
+    }
 }
