e35482ac071d04d87552bf4e9d8683b8217e98b3
egecoskun121 <77891949+egecoskun121@users.noreply.github.com>
2022-07-04 16:17:56 +0300

    Added Specialties Service and Map.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.services.CrudService;
+import org.springframework.stereotype.Service;
+
+
+public interface SpecialitiesService extends CrudService<Speciality,Long> {
+}
